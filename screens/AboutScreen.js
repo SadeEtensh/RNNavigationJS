b4af9b70ac0d626fd408ada02f72adc42f1dfbe1
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-11 14:28:22 +0300

    Passing data between screens

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,13 +1,22 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
 
-export default function () {
-  const navigation = useNavigation();
+export default function ({ route, navigation }) {
+  const { name } = route.params;
+  //   const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text style={styles.headingText}>About Screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Text style={styles.headingText}>About {name}</Text>
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate("Home")} /> */}
+      <Button
+        title="Update the name"
+        onPress={() =>
+          navigation.setParams({
+            name: "myYTChannel",
+          })
+        }
+      />
     </View>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, View, Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
 import React from "react";
 
-export default function HomeScreen() {
-  const navigation = useNavigation();
+export default function HomeScreen({ navigation }) {
+  //   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.headingText}>HomeScreen</Text>
       <Button
         title="Go to About"
-        onPress={() => navigation.navigate("About")}
+        onPress={() =>
+          navigation.navigate("About", {
+            name: "Sade",
+          })
+        }
       />
     </View>
   );
