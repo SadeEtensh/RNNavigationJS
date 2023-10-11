acc2140a8e88ef5c175842ec9ca02e981eeefa36
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-11 12:29:03 +0300

    RN navigation using hooks About page

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function () {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.headingText}>About Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 }
