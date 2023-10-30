54ad1dea524f517d158ce2a98ab4f62fc7d37661
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-30 16:59:36 +0300

    Bottom Nav Options

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import HomeScreen from "./screens/HomeScreen";
 import SettingScreen from "./screens/SettingScreen";
 import ProfileScreen from "./screens/ProfileScreen";
@@ -11,11 +12,24 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
-          tabBarLabelPosition: "beside-icon", // For iPad and Tablets
+          // tabBarLabelPosition: "beside-icon", // For iPad and Tablets
+          // tabBarShowLabel: false, //true by default
+          tabBarActiveTintColor: "red",
+          // tabBarInactiveTintColor: "red", //grey-by default
         }}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "My Profile", //header title remains the same
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+            tabBarBadge: 3, // notification or inbox
+          }}
+        />
         <Tab.Screen name="Settings" component={SettingScreen} />
       </Tab.Navigator>
     </NavigationContainer>
